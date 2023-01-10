c610f7f27008dd1f4bc5b9a7a71df49c131010e7
belizario87 <leonardobelizario88>
2023-01-09 23:51:05 -0300

    commits dos exercicios

---
+++ src/exercises/book/java/comoprogramar/capitulo2/Exercicio15.java
@@ -0,0 +1,39 @@
+package exercises.book.java.comoprogramar.capitulo2;
+
+
+import java.util.Scanner;
+
+/* 2.15 - (Aritimetica) Escreva um aplicativo que solicite ao usuario inserir dois inteiros,
+obtenha dele esses numeros e imprima sua soma, produto, diferenca e quociente (divisao).
+Utilize as tecnicas usadas na figura 2.7. (pag 36)
+*/
+public class Exercicio15 {
+
+
+    public static void main(String[] args) {
+        Scanner inputNumber = new Scanner(System.in); // Criando o objeto Scanner para o input do user
+
+        System.out.print("Escreva um numero:");
+        int number1 = inputNumber.nextInt();  // le o primeiro numero
+
+        System.out.print("Escreva um numero:");
+        int number2 = inputNumber.nextInt(); // le o segundo numero
+
+
+        int sum = number1 + number2;
+        System.out.println("A soma dos numeros e = " + sum); //exibe a soma
+
+        int prod = number1 * number2;
+        System.out.println("O produto dos numeros e = " + prod); // exibe o produto
+
+        int dif = number1 - number2;
+        System.out.println("A diferenca dos numeros e = " + dif); // exibe a subtracao
+
+        int quo = number1 / number2;
+        System.out.println("O quociente dos numeros e = " + quo); //exibe a divisao
+
+
+
+
+    }
+}
