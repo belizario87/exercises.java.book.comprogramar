6f88dcc41136346afbe75efe377e831cdfd6a626
belizario87 <leonardobelizario88>
2023-01-13 11:21:18 -0300

    add exercicios 18, 19

---
+++ src/exercises/book/java/comoprogramar/capitulo2/Exercicio18.java
@@ -0,0 +1,19 @@
+package exercises.book.java.comoprogramar.capitulo2;
+
+/*2.18 -> (Exibindo formas com asteriscos) Escreva um aplicativo que exiba uma caixa, 
+uma elipse, uma seta e um losango utilizando asteriscos(*), como segue: */
+
+public class Exercicio18 {
+  public static void main(String[] args) {
+    System.out.println("*********   ***    *       * ");
+    System.out.println("*       *  *   *  ***     *  *");
+    System.out.println("*       *  *   * *****   *    *");
+    System.out.println("*       *  *   *   *    *      *");
+    System.out.println("*       *  *   *   *   *        *");
+    System.out.println("*       *  *   *   *    *      *");
+    System.out.println("*       *  *   *   *     *    *");
+    System.out.println("*       *  *   *   *      *  *");
+    System.out.println("*********   ***    *       *");
+  }
+
+}
---
+++ src/exercises/book/java/comoprogramar/capitulo2/Exercicio19.java
@@ -0,0 +1,9 @@
+package exercises.book.java.comoprogramar.capitulo2;
+/*O que o seguinte codigo imprime ? 
+System.out.printf("*%n**%n***%n****%n"); */
+public class Exercicio19 {
+public static void main(String[] args) {
+  System.out.printf("*%n**%n***%n****%n");
+}
+  
+}
