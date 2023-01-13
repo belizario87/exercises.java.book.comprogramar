957b5cdeeedc228f3e68637be11933ec908dea5c
belizario87 <leonardobelizario88>
2023-01-13 11:15:13 -0300

    add exercicio 18, 19 e 20

---
+++ src/exercises/book/java/comoprogramar/capitulo2/Exercicio20.java
@@ -0,0 +1,18 @@
+package exercises.book.java.comoprogramar.capitulo2;
+/*O que o seguinte codigo imprime ? 
+System.out.println("*");
+System.out.println("***");
+System.out.println("*****");
+System.out.println("****");
+System.out.println("**"); */
+
+public class Exercicio20 {
+public static void main(String[] args) {
+  System.out.println("*");
+  System.out.println("***");
+  System.out.println("*****");
+  System.out.println("****");
+  System.out.println("**");
+}
+  
+}
